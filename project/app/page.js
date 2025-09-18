d3c58e8024b32bc2a9d7e6cea6dc45fa92077316
anonymous298 <tackletalha@gmail.com>
2025-09-19 01:00:23 +0500

    page.js created

--- project/app/page.js
+++ project/app/page.js
@@ -1,11 +1,23 @@
+import CategoriesSection from "@/components/CategoriesSection";
+import Hero from "@/components/Hero";
+import NewArrivals from "@/components/NewArrivals";
+import PopularProducts from "@/components/PopularProducts";
 import Image from "next/image";
-
-
-
 
 export default function Home() {
   return (
     <div>
+      {/* Hero Component */}
+      <Hero />
+
+      {/* New Arrival Component */}
+      <NewArrivals />
+
+      {/* Categories section */}
+      <CategoriesSection />
+
+      {/* Popular Products section */}
+      <PopularProducts />
 
     </div>
   );
